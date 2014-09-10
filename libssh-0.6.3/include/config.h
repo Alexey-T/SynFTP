0761e8c14cd07551f839bdb996f37f2cd6def6f1
tbeu <tc@tbeu.de>
2014-09-10 11:41:01 +0200

    Update libssh to v0.6.3

---
+++ libssh-0.6.3/include/config.h
@@ -0,0 +1,114 @@
+/************************** HEADER FILES *************************/
+
+/* Define to 1 if you have the <argp.h> header file. */
+//#cmakedefine HAVE_ARGP_H 1
+
+/* Define to 1 if you have the <pty.h> header file. */
+//#cmakedefine HAVE_PTY_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+//#cmakedefine HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <openssl/aes.h> header file. */
+#define HAVE_OPENSSL_AES_H 1
+
+/* Define to 1 if you have the <wspiapi.h> header file. */
+#define HAVE_WSPIAPI_H 1
+
+/* Define to 1 if you have the <openssl/blowfish.h> header file. */
+#define HAVE_OPENSSL_BLOWFISH_H 1
+
+/* Define to 1 if you have the <openssl/des.h> header file. */
+#define HAVE_OPENSSL_DES_H 1
+
+/*************************** FUNCTIONS ***************************/
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the `_snprintf' function. */
+#define HAVE__SNPRINTF 1
+
+/* Define to 1 if you have the `_snprintf_s' function. */
+#define HAVE__SNPRINTF_S 1
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if you have the `_vsnprintf' function. */
+#define HAVE__VSNPRINTF 1
+
+/* Define to 1 if you have the `_vsnprintf_s' function. */
+#define HAVE__VSNPRINTF_S 1
+
+/* Define to 1 if you have the `isblank' function. */
+#define HAVE_ISBLANK 1
+
+/* Define to 1 if you have the `strncpy' function. */
+#define HAVE_STRNCPY 1
+
+/* Define to 1 if you have the `cfmakeraw' function. */
+#define HAVE_CFMAKERAW 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
+#define HAVE_GETADDRINFO 1
+
+/* Define to 1 if you have the `poll' function. */
+//#cmakedefine HAVE_POLL 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `clock_gettime' function. */
+//#cmakedefine HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the `ntohll' function. */
+#define HAVE_NTOHLL 1
+
+/* Define to 1 if you have the `htonll' function. */
+#define HAVE_HTONLL 1
+
+/* Define to 1 if you have the `strtoull' function. */
+#define HAVE_STRTOULL 1
+
+/* Define to 1 if you have the `__strtoull' function. */
+#define HAVE___STRTOULL 1
+
+/* Define to 1 if you have the `_strtoui64' function. */
+#define HAVE__STRTOUI64 1
+
+/*************************** LIBRARIES ***************************/
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
+//#cmakedefine HAVE_LIBGCRYPT 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/**************************** OPTIONS ****************************/
+
+/* Define to 1 if you want to enable ZLIB */
+#define WITH_LIBZ 1
+
+/* Define to 1 if you want to enable SFTP */
+#define WITH_SFTP 1
+
+/* Define to 1 if you want to enable SSH1 */
+#define WITH_SSH1 1
+
+/************************* MS Windows ***************************/
+
+#ifdef _WIN32
+# ifdef _MSC_VER
+/* On Microsoft compilers define inline to __inline on all others use inline */
+#  undef inline
+#  define inline __inline
+
+#  undef strdup
+#  define strdup _strdup
+# endif // _MSC_VER
+#endif /* _WIN32 */
+
